aab57368723297245d08a5b95122ae67c051460e
ED laff <laffont.edu@gmail.com>
2024-09-30 12:42:28 +0200

    AAA commit

--- Assets/Script/Matt_change.cs
+++ Assets/Script/Matt_change.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Matt_change : MonoBehaviour
 {
     private float counter = 1f;
     public MeshRenderer a;
-    public Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector3(0, 10f, 0);
             if (counter == 1)
             {
                 a.material.color = Color.magenta;
@@ -28,27 +33,7 @@ public class Player : MonoBehaviour
                 a.material.color = Color.yellow;
             }
 
-            if (counter == 4)
-            {
-                a.material.color = Color.green;
-            }
-
-            if (counter == 5)
-            {
-                a.material.color = Color.cyan;
-            }
-
-            if (counter == 6)
-            {
-                a.material.color = Color.blue;
-            }
-
             counter = counter + 1;
-            if (counter==7)
-            {
-                counter = 1;
-            }
         }
     }
-    
 }
